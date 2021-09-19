b4cc4d62774e30ba901ebca27fdb5cc06037c1c6
Caio Fernandes <caiovf13@gmail.com>
2021-09-19 19:17:30 -0300

    📦 NEW: Add Informacao and Glossario models

--- documents/models.py
+++ documents/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-class Document(models.Model):
-    title = models.CharField("Title", max_length=240)
-    url = models.CharField("Url", max_length=1024)
-    description = models.CharField("Title", max_length=240)
-    created_at = models.DateField(auto_now_add=True)
+class Informacao(models.Model):
+    fonte = models.CharField(max_length=240)
+    data_extracao = models.DateField(auto_now_add=True)
+    titulo = models.CharField(max_length=240)
+    teor_textual = models.CharField(max_length=2000)
+    autor = models.CharField(max_length=240)
+    dataPublicacao = models.DateField()
 
     def __str__(self):
         return self.name
+
+
+class Glossario(models.Model):
+    palavra_chave = models.CharField(max_length=240)
+    created_at = models.DateField(auto_now_add=True)
